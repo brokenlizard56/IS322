4b3d6badefd41a2ac0fd574b9c2e52418051333b
Jose Baez <jlb22@@njit.edu>
2014-02-04 15:41:37 -0500

    added image gallery and related js

--- homework1/script.js
+++ homework1/script.js
@@ -6,18 +6,42 @@ $(w).resize(function()
 });
   
 //Check if Mobile
-//function checkMobile()
-//{
-//	mobile = (sw > breakpoint) ? false : true;
-//  if (!mobile) 
-//  { 
-//  	$('[role="tabpanel"],#nav,#search').show(); //Show full navigation and search
-//  } 
-//  else 
-//  { 
-//  	if(!$('#nav-anchors a').hasClass('active'))
-//  	{
-//    	$('#nav,#search').hide(); //Hide full navigation and search
-//    }
-//  }
-//}
+function checkMobile()
+{
+	mobile = (sw > breakpoint) ? false : true;
+	if (!mobile) 
+	{ 
+		$('[role="tabpanel"],#nav,#search').show(); //Show full navigation and search
+	} 
+	else 
+	{ 
+		if(!$('#nav-anchors a').hasClass('active'))
+		{
+			$('#nav,#search').hide(); //Hide full navigation and search
+		}
+	}
+}
+function buildGallery()
+{
+	container.html('<div id="img-list"><ul /></div>');
+  imgList = $('#img-list');
+  nav.find('a:first').addClass('active');
+  nav.find('a').each(function()
+	{
+		var $this = $(this);
+		var href = $this.attr('href');
+  	arr += '<li data-imgsrc="'+href+'"></li>';
+ 	});
+  imgList.find('ul').append(arr);
+  nav.on('click', 'a', function(e)
+	{
+  	var pos = $(this).parent().index();
+		e.preventDefault();
+		loadImg(pos);
+ 		if(swipeEnabled)
+		{
+  		mySwipe.slide(index, 300);
+ 		}
+ 		updateNav(pos);
+ 	});
+}
